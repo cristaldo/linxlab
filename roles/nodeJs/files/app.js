a0a2f0f09764431fe2bd93d9585a234e774563aa
Rafael Cristaldo! <rafael@rafaelcristaldo.com.br>
2020-03-17 18:00:33 -0300

    Node code updated

---
+++ roles/nodeJs/files/app.js
@@ -0,0 +1,34 @@
+
+var express = require('express');
+var app = express();
+var cluster = require('cluster'), os = require('os');
+
+   if (cluster.isMaster) {
+	var cpus = os.cpus().length;
+	for (var i = 0; i <= cpus; i++) {
+		cluster.fork();
+		}
+
+	cluster.on('listening', function(worker) {
+		console.log('Cluster %d esta conectado.', worker.process.pid);
+	});
+
+	cluster.on('disconnect', function(worker) {
+		console.log('Cluster %d esta desconectado.', worker.process.pid);
+	});
+
+	cluster.on('exit', function(worker) {
+		console.log('Cluster %d caiu fora.', worker.process.pid);
+	});
+
+ } else {
+
+	app.get('/', function (req, res) {
+		res.send('Hello World!');
+	});
+
+	app.listen(3000, function () {
+		console.log('Example app listening on port 3000!');
+	});
+
+}
